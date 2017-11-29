0b1caf71a68366e8db506738a832d70899fac754
ptflp <filippov.pa@s-vfu.ru>
2017-11-29 15:56:09 +0900

    test express js webserver

---
+++ notes-app/server/app.js
@@ -0,0 +1,10 @@
+import express from 'express';
+
+const app= express();
+
+app.get('/',(req, res)=> {
+	res.send('Hello world');
+})
+const server = app.listen(8080, ()=> {
+	console.log('Server is up and running on port 8080');
+})
