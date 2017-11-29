242853731e1ecb892a9603a75ccc6452c99a9a08
ptflp <filippov.pa@s-vfu.ru>
2017-11-29 22:45:33 +0900

    divided to components added less styles

--- notes-app/client/components/App.jsx
+++ notes-app/client/components/App.jsx
@@ -1,8 +1,19 @@
 import React from 'react';
-
+import NoteEditor from './NoteEditor.jsx';
+import NotesGrid from './NotesGrid.jsx';
+import './App.less';
 const App = React.createClass({
+	handleNoteAdd(data){
+		console.log(data);
+	},
 	render() {
-		return <h1> Notess asfssaf as</h1>;
+		return (
+		<div className="App">
+			<h2 className="App_header">NotesApp</h2>
+			<NoteEditor onNoteAdd={this.handleNoteAdd}/>
+			<NotesGrid />
+		</div>
+			)
 	}
 })
 
---
+++ notes-app/client/components/Note.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Note = React.createClass({
+	render() {
+		return <h1> Notes</h1>;
+	}
+})
+
+export default Note;
