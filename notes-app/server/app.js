3f4f6bbe7c914c35da2390ec86fd201492e9b658
ptflp <filippov.pa@s-vfu.ru>
2017-11-29 16:33:35 +0900

    adding config json

--- notes-app/server/app.js
+++ notes-app/server/app.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-import * as db from './utils/DataBaseUtils.js'
+import {serverPort} from '../config/config.json';
+import * as db from './utils/DataBaseUtils.js';
 db.setUpConnection();
 
 const app= express();
@@ -8,17 +9,17 @@ const app= express();
 app.use(bodyParser.json());
 
 app.get('/notes',(req, res)=> {
-	db.listNotes().then(data => res.send(data))
-})
+	db.listNotes().then(data => res.send(data));
+});
 
 app.post('/notes',(req, res)=> {
 	db.createNote(req.body).then(data=>res.send(data));
-})
+});
 
 app.delete('/notes:id',(req, res)=> {
-	db.deleteNote(req.params.id).then(data => res.send(data))
-})
+	db.deleteNote(req.params.id).then(data => res.send(data));
+});
 
-const server = app.listen(8080, ()=> {
-	console.log('Server is up and running on port 8080');
-})
+const server = app.listen(serverPort, ()=> {
+	console.log(`Server is up and running on port ${serverPort}`);
+});
--- notes-app/server/utils/DataBaseUtils.js
+++ notes-app/server/utils/DataBaseUtils.js
@@ -2,10 +2,14 @@ import mongoose from "mongoose";
 
 import '../models/Note';
 
+import config from '../../config/config.json';
+
+
 const Note = mongoose.model('Note');
+const db = `mongodb://${config.db.host}:${config.db.port}/${config.db.name}`;
 
 export function setUpConnection() {
-	mongoose.connect('mongodb://NODEREM_mongo/notes');
+	mongoose.connect(db);
 }
 
 export function listNotes() {
